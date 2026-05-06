206a3a8c8f50ab4814e253d83de4380113a8f85d
marcelopenas <marcelopenas@outlook.com>
2026-05-06 13:50:23 -0300

    add pytest to build step

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
